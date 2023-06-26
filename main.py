a8b0cee67226c482154c7d10246c4b8b22719cce
SophiaSun18 <sophiasza18@gmail.com>
2023-06-26 16:54:37 -0400

    update fastapi post data

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,17 +19,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Item(BaseModel):
+    data: str
+
 @app.get("/")
 async def root():
     return {"Hello! Welcome to dev6!"}
 
-@app.get("/info")
-async def get_info():
+@app.get("/repsponse")
+async def get_response():
     return json.loads(open("testInfo.json").read())
 
 @app.get("/directory/{path:path}", response_class=FileResponse)
 async def access_contents(path:str):
     return path
+
+@app.post("/data")
+async def post_data(data: Item):
+    return "Success: data is sent!"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(sys.argv[1]))
