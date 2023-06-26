3df87b7625430e88da8e3ffe41c1f27d2a130487
SophiaSun18 <sophiasza18@gmail.com>
2023-06-26 13:52:18 -0400

    fastapi update with content access

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import uvicorn
@@ -6,7 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 app = FastAPI()
-root_path = "/OfficeAddIn_addCommentToDoc/assets/"
 
 origins = ["*"]
 
@@ -18,17 +18,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.get("/")
-# async def root():
-#     return {"Hello World!"}
+@app.get("/")
+async def root():
+    return {"Hello! Welcome to dev6!"}
 
 @app.get("/info")
 async def get_info():
     return json.loads(open("testInfo.json").read())
 
-@app.get("/", response_class=FileResponse)
-async def root():
-    return root_path
+# @app.get("/", response_class=FileResponse)
+# async def root():
+#     return root_path
+
+@app.get("/directory/{path:path}")
+async def access_contents(path:str):
+    directory_path = os.path.join(os.getcwd(), path)
+    if (os.path.isdir(directory_path)):
+        contents = os.listdir(directory_path)
+    elif (os.path.isfile(directory_path)):
+        content = directory_path
+    return content
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(sys.argv[1]))
